0993522c357f1c836a70a5582051b4a43f9713a6
adrimr02 <adrianmartinezr2002@gmail.com>
2023-03-10 22:28:02 +0100

    Estilos de LoginPage migrados a styled-components

--- webapp/src/pages/LoginPage/index.tsx
+++ webapp/src/pages/LoginPage/index.tsx
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from 'react'
 
-import { Autocomplete, TextField, Divider, Box } from '@mui/material';
+import { Autocomplete, TextField, Divider, Box } from '@mui/material'
+import styled from 'styled-components'
 
 import { login } from "@inrupt/solid-client-authn-browser";
-import './login.css'
 import { Button } from '@mui/material';
-
+import './login.css'
 
 const Login = ({  }) : JSX.Element => {
 
-  const [identity, setIdentity] = useState<ProviderOption | null>({ label: '', url: '' });
+  const [identity, setIdentity] = useState<ProviderOption | null>({ label: '', url: '' })
 
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
+    event.preventDefault()
     
     console.log(identity)
     if (identity) {
@@ -27,7 +27,7 @@ const Login = ({  }) : JSX.Element => {
   };
   
   return (
-    <div className='container'>
+    <Container>
       {/* Div imagenes(izquierda) */}
       <div className='column'>
         <h1 className='title'>LoMap</h1>
@@ -46,21 +46,21 @@ const Login = ({  }) : JSX.Element => {
       {/* Div formulario(derecha) */}
       <div className='column'>
         <h1 className='title'>LoMap</h1>
-        <form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit}>
           <h2>Elige un proveedor</h2>
           <Autocomplete
-          disablePortal
-          options={proveedores}
-          getOptionLabel = {option => option.label}
-          sx={{ width: 300 }}
-          value={identity}
-          onChange={(event, value : ProviderOption | null) => setIdentity(value)}
-          renderInput={(params) => <TextField {...params} label="Proveedor"/>}
+            disablePortal
+            options={providers}
+            getOptionLabel = {option => option.label}
+            sx={{ width: 300 }}
+            value={identity}
+            onChange={(event, value : ProviderOption | null) => setIdentity(value)}
+            renderInput={(params) => <TextField {...params} label="Proveedor"/>}
           />
-          <Button type="submit" variant="contained">Registrarse</Button>
-        </form> 
+          <Button type="submit" variant="contained">Entrar</Button>
+        </Form> 
       </div>
-    </div>
+    </Container>
   )
 }
 
@@ -69,9 +69,75 @@ interface ProviderOption {
   url: string
 }
 
-const proveedores : ProviderOption[] = [
+const providers : ProviderOption[] = [
   {label: 'SOLID', url: 'https://solidcommunity.net/'},
   {label: 'INRUPT', url: 'https://inrupt.net/'}
 ]
+
+const Container = styled.div`
+  padding: 0.5em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+
+  .column {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    flex: 50%;
+
+    .title {
+      font-size: 5em;
+      text-align: center;
+      color: white;
+      margin: 0.2em 0;
+      font-family: 'Oswald', sans-serif;
+    }
+
+    .subtitle {
+      font-size: 3em;
+      text-align: center;
+      color: white;
+      font-family: 'Oswald', sans-serif;
+      margin: 0.2em 0;
+    }
+  }
+
+  .column:nth-child(3) > .title {
+    display: none;
+  }
+
+  .divider {
+    background-color: #d3d3d3;
+  }
+`
+
+const Form = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #eee;
+  border-radius: 0.5em;
+  padding: 0.5em;
+  padding-bottom: 1em;
+  -webkit-box-shadow: 8px 8px 10px 0px rgba(34,2,0,0.27);
+  -moz-box-shadow: 8px 8px 10px 0px rgba(34,2,0,0.27);
+  box-shadow: 8px 8px 10px 0px rgba(34,2,0,0.27);
+
+  > * {
+    margin-bottom: 0.6em;
+    width: 90%;
+  }
+
+  > h2 {
+    text-align: center;
+    margin: 0;
+    font-size: 1.7em;
+    margin-bottom: 0.5em;
+  }
+`
 
 export default Login
