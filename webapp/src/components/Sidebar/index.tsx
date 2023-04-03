ba8e5ee2fa792ce9b5575ec280776611ab251ab7
UO276803 <UO276803@uniovi.es>
2023-04-03 19:33:40 +0200

    Primera version de la barra como popup

--- webapp/src/components/Sidebar/index.tsx
+++ webapp/src/components/Sidebar/index.tsx
@@ -4,9 +4,9 @@ import { FaBars, FaTimes, FaChevronRight } from "react-icons/fa"
 import { useMap } from "react-map-gl";
 import { MarkerContext } from "../../context/MarkersContext";
 import { IMarker } from "../../types/IMarker";
-import { ClosedSidebar, CloseSection, MarkerHover, MarkerSection, SearchBar, SidebarSection, Title, TopSection } from "./Styles"
+import { MarkerHover, MarkerSection, SearchBar, Title, TopSection } from "./Styles"
 import styled, { keyframes } from 'styled-components'
-
+import SidePopup from '../SidePopup';
 
 const Sidebar = () => {
 
@@ -31,42 +31,30 @@ const Sidebar = () => {
 
   return (
     <>
-      {
-        isOpen ?
-        <SidebarSection>
-          <TopSection>
-            <Title>Puntos de interés</Title>
-            <CloseSection>
-              <FaTimes onClick={toggleSidebar}/>
-            </CloseSection>
-          </TopSection>
-          <div>
-            <SearchBar isOpen={isOpen} type="text" placeholder="Buscar" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
-          </div>
-          <List
-            sx={{
-              width: '100%',
-              height: '100%',
-              maxWidth: 360,
-              bgcolor: '#f8f8f8',
-              position: 'relative',
-              overflow: 'auto',
-              maxHeight: isOpen ? 400 : 450
-            }}
-          >
-            {
-            filteredMarkers.map((marker) => (
-              <Marker key={marker.id} marker={marker} onClick={handleMarkerClick} />
-            ))
-            }
-          </List>
-        </SidebarSection>
-        :
-
-        <ClosedSidebar>
-          <FaChevronRight onClick={toggleSidebar}/>
-        </ClosedSidebar>
-      }
+      <SidePopup isOpen={isOpen} closePopup={toggleSidebar}>
+        <Title>Puntos de interés</Title>
+        <TopSection>
+          <SearchBar isOpen={isOpen} type="text" placeholder="Buscar" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} />
+        </TopSection>
+        <List
+          sx={{
+            width: '100%',
+            height: '100%',
+            maxWidth: 360,
+            bgcolor: '#f8f8f8',
+            position: 'relative',
+            overflow: 'auto',
+            maxHeight: isOpen ? 400 : 450
+          }}
+        >
+          {
+          filteredMarkers.map((marker) => (
+            <Marker key={marker.id} marker={marker} onClick={handleMarkerClick} />
+          ))
+          }
+        </List>
+      </SidePopup>
+      <FaChevronRight onClick={toggleSidebar}/>
     </>
   )
 }
