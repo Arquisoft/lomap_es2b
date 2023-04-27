ade4287de47da132b7d9a36090d832ae56772fdc
UO282823 <UO282823@uniovi.es>
2023-04-27 21:57:48 +0200

    Añadir noticias(no funciona)

--- webapp/src/components/NewsPopup/Styles.tsx
+++ webapp/src/components/NewsPopup/Styles.tsx
@@ -4,9 +4,25 @@ export const Container= styled.div`
     .new{
         background-color: #FFFFFF;
         color: #000000;
-        border: 0.1em solid #000;
+        border: 0.15em solid #03042aac;
         padding: 1em;
         border-radius: 0.7em;
         margin-bottom:1em;
     }
+    
+`
+
+export const AddForm = styled.form`
+
+    .addNewsButton{
+
+    }
+    .textField{
+        display: block;
+    }
+    
+`
+
+export const Error = styled.p`
+    color: red;
 `
--- webapp/src/components/NewsPopup/index.tsx
+++ webapp/src/components/NewsPopup/index.tsx
@@ -1,8 +1,12 @@
 import { useContext, useState } from "react";
 import { INews } from "../../types/INews";
 import { NewsContext } from "../../context/NewsContext";
-import { Container } from "./Styles";
+import { AddForm, Container,Error } from "./Styles";
 import Popup from "../PopUp";
+import { Types } from "../../types/ContextActionTypes";
+import { useSession } from "@inrupt/solid-ui-react";
+import { v4 as uuid } from 'uuid'
+import { Button, TextField } from "@mui/material";
 
 interface Props{
     toggleNews:(open: boolean | undefined) => void
@@ -18,17 +22,38 @@ function NewsPopup({isNewsOpen, toggleNews } : Props){
     {id:"1",text:"prueba1",author:"autor1S"},
     {id:"1",text:"pruecsdddddddccddcsdcsdcrgtehryhtegfwefcrthgyreeargtraergeargerfba1",author:"autor1S"}]
 
+    const {session} = useSession();
+    const [showPopupNews,setShowPopupNews] = useState<boolean>(false);
+
 
     function showNews(){
         return(
             <>
-            {pruebaNews.length!==0 ? 
-            pruebaNews.map((n,index)=>(
+            {news.length!==0 ? 
+            news.map((n,index)=>(
                 <New key={index} {...n} />
             )):
             <p>Aquí se mostrarán las noticias</p> }
         </>
         )
+    }
+
+    function addNew(text:string){
+        if(!session.info.webId) return
+        console.log("Llega al addNew, text:"+text)
+        dispatch({ type: Types.ADD, payload: { news: {id:uuid(),text:text,author:session.info.webId} } })
+        console.log(news)
+    }
+
+    function showAddNewPopup(){
+        toggleNews(false);
+        setShowPopupNews(true);
+    }
+
+    function closeAddNewPopup(){
+        setShowPopupNews(false);
+        toggleNews(true);
+        
     }
 
     return(
@@ -39,7 +64,10 @@ function NewsPopup({isNewsOpen, toggleNews } : Props){
             <h2>Noticias</h2>
             {showNews()}
             </Container>
+            <Button style={{float:'right'}} onClick={showAddNewPopup} color='success' variant='contained'>Añadir noticia</Button>
          </Popup>
+        : showPopupNews ? 
+            <AddNewsPopup addNew={addNew} onClose={closeAddNewPopup } />
         : null}
         </> 
     )
@@ -55,6 +83,52 @@ const New = (inew:INews)=>{
     )
 }
 
+interface PropsAddNewsPoup{
+    onClose:()=>void
+    addNew:(text:string)=>void
+}
+
+const AddNewsPopup = ({onClose, addNew}:PropsAddNewsPoup)=>{
+    const [text,setText] = useState<string>("");
+    const maxLong = 500
+    const [error,isError] = useState<boolean>(false)
+
+    function handleChangeText(newText:string){
+        if(newText.length>maxLong){
+          return;
+        }
+        isError(false)
+        setText(newText)
+      }
+
+      function handleSubmit(e:React.FormEvent){
+        e.preventDefault();
+        if(text.trim().length===0){
+            isError(true);
+            return
+        }else{
+            isError(false);
+        }
+        addNew(text)
+        onClose();
+      }
+
+    return(
+        <Popup isOpen={true} closePopup={onClose}>
+             <AddForm onSubmit={(e)=>handleSubmit(e)}>
+            <h2>Introduce la noticia</h2>
+            <p><label htmlFor="Text">Texto:</label></p>
+            <TextField className="textField" helperText={text.length+"/"+maxLong} onChange={(e)=>handleChangeText(e.target.value)} value={text} id='Text' label="Noticia" variant='standard' multiline maxRows={4} />  
+            {error ? <Error>El texto no puede estar vacío</Error>: null}
+            <Button className="addNewsButton" type='submit' color='success' variant='contained'>Añadir noticia</Button>
+            </AddForm>
+        </Popup>
+       
+    )
+}
+
 
 
 export default  NewsPopup;
+
+
